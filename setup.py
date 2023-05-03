ee5ae1227d07e69b0bfa0ca772e105ac9df54b3b
Shah-Charmi <98964159+Shah-Charmi@users.noreply.github.com>
2023-05-03 19:43:53 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+## edit below variables as per your requirements -
+REPO_NAME = "Books-Recommender-System-Using-Machine-Learning"
+AUTHOR_USER_NAME = "ShahCharmi"
+SRC_REPO = "src"
+LIST_OF_REQUIREMENTS = ['streamlit', 'numpy']
+
+
+setup(
+    name=SRC_REPO,
+    version="0.0.1",
+    author=AUTHOR_USER_NAME,
+    description="A small package for Book Recommender System",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/Shah-Charmi/recommender_system_book",
+    author_email="shahcharmi144@gmail.com",
+    packages=[SRC_REPO],
+    license="MIT",
+    python_requires=">=3.7",
+    install_requires=LIST_OF_REQUIREMENTS
+)
